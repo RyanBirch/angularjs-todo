d52f05aca2efd24d35e35c7973286023190cd3d1
Ryan Birch <ryan.birch@gmail.com>
2019-12-30 19:29:06 -0500

    able to save todos in localStorage

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -2,10 +2,30 @@ const todoList = angular.module('todoList', [])
 
 todoList.controller('todoCtrl', function($scope) {
 
-  $scope.addTodo = function(newTodo) {
-    console.log(newTodo)
+  // check if we have any todos in localStorage
+  try {
+    let arr = JSON.parse(localStorage.getItem('savedTodos'))
+    if (arr) $scope.savedTodos = arr
+    else $scope.savedTodos = []
+  } catch(error) {
+    console.log(error)
+  }
 
-    // need to add todos to an array in local storage
+  // add a todo item
+  $scope.addTodo = function(newTodo) {
+    
+    // save new todo in newTodo array
+    $scope.savedTodos.push({
+      task: newTodo,
+      done: false
+    })
+
+    // put new todo in localStorage
+    localStorage.clear()
+    localStorage.setItem('savedTodos', JSON.stringify($scope.savedTodos))
+    
+    // clear the input field
+    $scope.newTodo = ''
   }
 
 })
